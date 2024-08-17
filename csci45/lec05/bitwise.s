d7ef942421447b065e60e2d4c54601e78e9f864e
Lawton Nichols <lawtonnichols@gmail.com>
2024-08-16 18:54:44 -0700

    .

---
+++ csci45/lec05/bitwise.s
@@ -0,0 +1,22 @@
+.global _start  // _start is a global label
+_start:
+  mov x0, #21 
+
+  // extract bit 2
+  mov x1, #4
+  and x2, x0, x1
+  // check if x2 == 0, ...
+
+  // set bit 5
+  mov x1, #32
+  orr x3, x0, x1
+
+  // negate using two's complement
+  mvn x4, x0
+  add x4, x4, #1
+
+  // exit (0)
+  mov w0, #0  // move the value to return into w0
+  mov x8, #93  // move 93 into the register x8
+  // 93 is the code for the "exit" system call
+  svc #0  // hey Linux kernel, please execute the system call that I just set up!
