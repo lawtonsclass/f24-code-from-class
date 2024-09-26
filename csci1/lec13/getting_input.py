37af1646dd9a67cb56a4da0903e2d97fd6dc4561
Lawton Nichols <lawtonnichols@gmail.com>
2024-09-25 17:11:20 -0700

    .

---
+++ csci1/lec13/getting_input.py
@@ -0,0 +1,10 @@
+resp = input('Enter your name: ')
+print(type(resp))
+print('Hi, ' + resp + '!')
+print('Hi,', resp, '!')
+
+num = input('Enter your favorite number: ')
+print(type(num))
+# print(num * 2)
+num_as_int = int(num) # num is only a str--we must convert it to an int!
+print(num_as_int * 2)
