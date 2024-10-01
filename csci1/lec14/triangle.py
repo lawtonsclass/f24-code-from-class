b4839e8eaa0d4e43e98ccd309c3235deac4492f0
Lawton Nichols <lawtonnichols@gmail.com>
2024-09-30 17:12:39 -0700

    .

---
+++ csci1/lec14/triangle.py
@@ -0,0 +1,31 @@
+import turtle
+
+print(turtle.position())
+
+# make the turtle actually look like a turtle
+turtle.shape("turtle")
+
+turtle.forward(200)
+
+turtle.left(180 - 60)
+
+turtle.forward(200)
+
+turtle.left(180 - 60)
+
+turtle.forward(200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
