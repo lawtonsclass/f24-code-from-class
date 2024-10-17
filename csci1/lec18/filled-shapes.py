61113efd861cfd7ed4886590a055feff7259d1c5
Lawton Nichols <lawtonnichols@gmail.com>
2024-10-16 17:06:28 -0700

    .

---
+++ csci1/lec18/filled-shapes.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.fillcolor('green')
+
+# start filling, please!
+turtle.begin_fill()
+
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.left(90)
+
+turtle.pencolor('blue')
+turtle.forward(100)
+turtle.left(90)
+
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.left(90)
+
+turtle.pencolor('blue')
+turtle.forward(100)
+
+turtle.end_fill()
