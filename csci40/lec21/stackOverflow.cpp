0d29bb195d9244b4c439ea7bb0f7958bfab8ce93
Lawton Nichols <lawtonnichols@gmail.com>
2024-10-24 15:21:35 -0700

    .

---
+++ csci40/lec21/stackOverflow.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// issue 1: forgets the base case
+int fact1(int n) {
+  cout << "inside fact1(" << n << ")" << endl;
+  return n * fact1(n-1);
+}
+
+// issue 2: recursive case does not make progress towards base case
+int fact2(int n) {
+  cout << "inside fact2(" << n << ")" << endl;
+  if (n == 1) {
+    // base case: 1! = 1
+    return 1;
+  } else {
+    // incorrect recursive case
+    return fact2(n);
+  }
+}
+
+int main() {
+  // cout << fact1(5) << endl;
+  cout << fact2(5) << endl;
+
+  return 0;
+}
