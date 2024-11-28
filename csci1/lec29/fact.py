071a1a779c9b578baf49f3a288f59e6120e78c71
Lawton Nichols <lawtonnichols@gmail.com>
2024-11-27 17:15:19 -0800

    .

---
+++ csci1/lec29/fact.py
@@ -0,0 +1,14 @@
+# computes n!
+def fact(n):
+    # print("Inside fact(" + str(n) + ")")
+    
+    if n == 1:
+        # base case
+        return 1
+    else:
+        # recursive case
+        x = fact(n-1)
+        return n * x
+
+print(fact(5))
+print(fact(3))
